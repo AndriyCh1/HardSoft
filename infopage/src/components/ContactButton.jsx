9577f02ad11a10162c495118025df179345ec1c7
AndriyCh1 <83582521+AndriyCh1@users.noreply.github.com>
2021-05-12 11:20:34 +0300

    Create ContactButton.jsx

---
+++ infopage/src/components/ContactButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css'
+
+const ContactButton = () =>{
+    return(
+        <a href="/" className="buttons__call">Сконтактувати</a>
+    )
+}
+
+export default ContactButton;
